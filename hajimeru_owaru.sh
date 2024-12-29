9f19c6576acf7e4eddd388fd527c0c7be19e93ba
Ryuta Kawano <ryuuta.kawano@gmail.com>
2024-12-29 18:48:19 +0900

    shscript deleted

--- hajimeru_owaru.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rsync -avz assets/ uploaded_files/
-rsync -avz assets hello uploaded_files /home/ryuta/ryuut/Documents/at25_bak
-
-refact=/home/ryuta/refenv/bin/activate
-# shellcheck source=/dev/null
-source ${refact}
