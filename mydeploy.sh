389e66d52d38aa43ef54204e5a67e44cd376f713
Ryuta Kawano <ryuuta.kawano@gmail.com>
2025-01-05 23:11:10 +0900

    deploy sh

---
+++ mydeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT=$(cd "$(dirname "$0")" || return ;pwd)
+cd uploaded_files/mp4s || return
+for f in *.mp4 ; do
+    rm -f "${f}"
+done
+
+cd "$CURRENT" || return
+pwd
+echo "$CURRENT"
+reflex deploy --project b88f7e50-9f73-4222-9e47-cb5f6a6f08ba
