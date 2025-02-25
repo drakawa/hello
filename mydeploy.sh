9fce234635342a3ca48785db2677bd3de435d3c4
Ryuta Kawano <ryuuta.kawano@gmail.com>
2025-02-25 14:43:31 +0900

    sync uploaded_files (denied by reflex cloud...)

--- mydeploy.sh
+++ mydeploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CURRENT=$(cd "$(dirname "$0")" || return ;pwd)
-cd uploaded_files/ || return
-for f in *.mp4 ; do
-    rm -f "${f}"
-done
+# CURRENT=$(cd "$(dirname "$0")" || return ;pwd)
+# cd uploaded_files/ || return
+# for f in *.mp4 ; do
+#     rm -f "${f}"
+# done
 
-cd "$CURRENT" || return
-pwd
-echo "$CURRENT"
+# cd "$CURRENT" || return
+# pwd
+# echo "$CURRENT"
 reflex deploy --project b88f7e50-9f73-4222-9e47-cb5f6a6f08ba
